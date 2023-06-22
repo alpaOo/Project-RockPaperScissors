660180b6a62f0e5aeb945f8180692f75db3929a9
John <128350391+alpaOo@users.noreply.github.com>
2023-06-23 00:59:05 +0800

    refactor: Refactored the code for readability, using object and array.

--- script.js
+++ script.js
@@ -1,49 +1,41 @@
-let playerText = document.querySelector("#playerText");
-let computerText = document.querySelector("#computerText");
-let resultsText = document.querySelector("#resultsText");
-const choiceButtons = document.querySelectorAll("#choiceButtons");
+let playerText = document.querySelector('#playerText');
+let computerText = document.querySelector('#computerText');
+let resultsText = document.querySelector('#resultsText');
+const choiceButtons = document.querySelectorAll('#choiceButtons');
 let playerSelection;
 let computerSelection;
-let results;
 
-choiceButtons.forEach(button => button.addEventListener('click', () => {
-	
-	playerSelection = button.textContent;
-	console.log(playerSelection);
-	getComputerChoice();
-	console.log(getComputerChoice());
+choiceButtons.forEach((button) =>
+  button.addEventListener('click', () => {
+    playerSelection = button.textContent;
+    // console.log(playerSelection);
+    getComputerChoice();
+    // console.log(getComputerChoice());
 
-	playerText.innerHTML = `Player: ${playerSelection}`;
-	computerText.innerHTML = `Computer: ${computerSelection}`;
-	getResult();
+    playerText.innerText = `Player: ${playerSelection}`;
+    computerText.innerText = `Computer: ${computerSelection}`;
+    getResult();
+  })
+);
 
-}));
+function getResult() {
+  const winConditions = {
+    ROCK: 'SCISSORS',
+    PAPER: 'ROCK',
+    SCISSORS: 'PAPER',
+  };
 
-function getResult(){
-	if(playerSelection === computerSelection){
-		resultsText.innerHTML = "Result: It's a DRAW.";
-	}	else if((playerSelection === "ROCK") && (computerSelection === "SCISSORS")){
-		resultsText.innerHTML = "Result: You WIN."
-	} 	else if((playerSelection === "PAPER") && (computerSelection === "ROCK")){
-		resultsText.innerHTML = "Result: You WIN."
-	}	else if((playerSelection === "SCISSORS") && (computerSelection === "PAPER")){
-		resultsText.innerHTML = "Result: You WIN."
-	}	else {
-		resultsText.innerHTML = "Result: You LOSE."
-	}
+  if (playerSelection === computerSelection) {
+    resultsText.innerText = `Result: It's a DRAW!`;
+  } else if (winConditions[playerSelection] === computerSelection) {
+    resultsText.innerText = `Result: You WIN!`;
+  } else {
+    resultsText.innerText = `Result: You LOSE!`;
+  }
 }
 
-function getComputerChoice(){
-	computerSelection = Math.floor(Math.random() * 3) + 1;
-	switch(computerSelection){
-		case 1:
-			computerSelection = "ROCK";
-			break;
-		case 2:
-			computerSelection = "PAPER";
-			break;
-		case 3:
-			computerSelection = "SCISSORS";
-			break;
-	}
+function getComputerChoice() {
+  const choices = ['ROCK', 'PAPER', 'SCISSORS'];
+  const randomValue = Math.floor(Math.random() * choices.length);
+  computerSelection = choices[randomValue];
 }
